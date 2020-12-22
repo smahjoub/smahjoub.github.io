2f9ff1864547de07605e37adf133ea605fccd860
Saifeddine Mahjoub <saifeddine.mahjoub@outlook.com>
2020-12-22 15:51:30 +0100

    minor fixes

--- js/main.js
+++ js/main.js
@@ -56,17 +56,15 @@
                 $.ajax({
                     type: "POST",
                     url: url,
-                    data: $(this).find('input[name!=g-recaptcha-response]').serialize(),
-                    success: function (data)
+                    dataType: "json",
+                    data: $(this).serialize(),
+                    success: function ()
                     {
-                        var messageAlert = 'alert-' + data.type;
-                        var messageText = data.message;
-
-                        var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                        if (messageAlert && messageText) {
-                            $('#contact-form').find('.messages').html(alertBox);
-                            $('#contact-form')[0].reset();
-                        }
+                        var messageText = 'Message was sent successfully!';
+                        var alertBox = '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                        
+                        $('#contact-form').find('.messages').html(alertBox);
+                        $('#contact-form')[0].reset();
                     }
                 });
                 return false;
